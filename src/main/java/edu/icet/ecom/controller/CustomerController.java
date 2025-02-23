a6ab0eab4e25f37730cfae61d892890171ba6c11
SasmithaHiram <sasmithahiram2003@gmail.com>
2025-02-23 19:46:02 +0530

    Implement Add Customer

--- src/main/java/edu/icet/ecom/controller/CustomerController.java
+++ src/main/java/edu/icet/ecom/controller/CustomerController.java
@@ -1,8 +1,23 @@
 package edu.icet.ecom.controller;
 
+import edu.icet.ecom.dto.Customer;
+import edu.icet.ecom.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/customer")
+@RequiredArgsConstructor
+@CrossOrigin
+
+
 public class CustomerController {
-    private Integer id;
-    private String name;
-    private String email;
-    private String phoneNumber;
+    final CustomerService service;
+
+    @PostMapping("/add-customer")
+    public void addCustomer(@RequestBody Customer customer) {
+        service.addCustomer(customer);
+        System.out.println(customer);
+    }
+
 }
--- src/main/java/edu/icet/ecom/service/CustomerService.java
+++ src/main/java/edu/icet/ecom/service/CustomerService.java
@@ -1,4 +1,9 @@
 package edu.icet.ecom.service;
 
+import edu.icet.ecom.dto.Customer;
+import edu.icet.ecom.entity.CustomerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface CustomerService {
+    void addCustomer(Customer customer);
 }
