afb404cbb46026d717ae476be55cd13b8eb3582e
SasmithaHiram <sasmithahiram2003@gmail.com>
2025-07-21 19:46:36 +0530

    set public path as user-create

--- src/main/java/edu/icet/ecom/configuration/SecurityConfig.java
+++ src/main/java/edu/icet/ecom/configuration/SecurityConfig.java
@@ -2,10 +2,27 @@ package edu.icet.ecom.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(csrf -> csrf.disable())
+                        .authorizeHttpRequests(
+                                auth -> auth
+                                        .requestMatchers("/user/**").permitAll()
+                        )
+                .httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
